4b8f31c7111b2434cf1de934f636451a0f9bd420
bravesimon <46894011+bravesimon@users.noreply.github.com>
2024-05-26 20:20:27 +0200

    added main site and menu refact

--- config/menu.php
+++ config/menu.php
@@ -22,12 +22,13 @@ $pages = array(
 	'/' => array('fajl' => 'cimlap', 'szoveg' => 'Címlap', 'menun' => array(1,1)),
 	'bemutatkozas' => array('fajl' => 'bemutatkozas', 'szoveg' => 'Bemutatkozás', 'menun' => array(1,1)),
 	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
-	'cikkek' => array('fajl' => 'cikkek', 'szoveg' => 'Cikkek', 'menun' => array(1,1)),
-    'tablazat' => array('fajl' => 'tablazat', 'szoveg' => 'Táblázat', 'menun' => array(1,1)),
+	'galeria' => array('fajl' => 'galeria', 'szoveg' => 'Galeria', 'menun' => array(1,1)),
+	'uzenetek' => array('fajl' => 'uzenetek', 'szoveg' => 'Uzenetek', 'menun' => array(0,1)),
+    'tablazat' => array('fajl' => 'tablazat', 'szoveg' => 'Táblázat', 'menun' => array(0,1)),
     'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés', 'menun' => array(1,0)),
     'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1)),
     'belep' => array('fajl' => 'belep', 'szoveg' => '', 'menun' => array(0,0)),
     'regisztral' => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0,0))
 );
-
+ 
 ?>
--- templates/pages/cimlap.tpl.php
+++ templates/pages/cimlap.tpl.php
@@ -9,3 +9,15 @@
     <li>Adatok lekerese</li>
     <li>Adatok modositasa</li>
 </p>
+
+<br><br><br><br><br>
+
+<h3>Video webhelyrol:</h3>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/a_wgcIKAZ-I?si=UeJOC2V0QcR2vxuw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+<h3>Video lokalis eleresbol:</h3>
+<video width="560" height="315" src="./videos/SampleVideo.mp4"></video>
+
+<h3>Webhely fizikai helye:</h3>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2726.3375296155727!2d19.66695091525771!3d46.89607994478184!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4743da7a6c479e1d%3A0xc8292b3f6dc69e7f!2sPallasz+Ath%C3%A9n%C3%A9+Egyetem+GAMF+Kar!5e0!3m2!1shu!2shu!4v1475753185783" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
